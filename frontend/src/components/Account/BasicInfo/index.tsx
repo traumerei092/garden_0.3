f971185d2180fef6100a99015ea0eaa28c849048
traumerei092 <dicek092@gmail.com>
2025-10-07 12:43:51 +0900

    BasicInfo/index.tsx修正

--- frontend/src/components/Account/BasicInfo/index.tsx
+++ frontend/src/components/Account/BasicInfo/index.tsx
@@ -249,7 +249,7 @@ const BasicInfo: React.FC<BasicInfoProps> = ({ userData, profileOptions, userAtm
       {/* プロフィール完成度 */}
       {userData && profileOptions && (
         <ProfileCompletion
-          userData={userData}
+          userData={userData as unknown as User}
           profileOptions={profileOptions}
           userAtmospherePreferences={userAtmospherePreferences || []}
         />
